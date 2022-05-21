0abf07e9e5e5a07560a09153e69174ea3536001b
betorendon <betorendonruiz@gmail.com>
2022-05-21 00:34:22 -0500

    Movie fetcher now inserts to DB instead of CSV

--- src/movies/movie_fetcher.py
+++ src/movies/movie_fetcher.py
@@ -1,19 +1,43 @@
+from flask import Flask, request
+from datetime import datetime
 import requests
 import re
 import csv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from flask_sqlalchemy import SQLAlchemy
 from bs4 import BeautifulSoup
+from movies import models
 
-from movies.models import get_postgres_uri
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = models.get_postgres_uri()
+app.config['SECRET_KEY'] = "B3TINSKY"
+models.start_mappers()
 
-DEFAULT_SESSION_FACTORY = sessionmaker(
-    bind=create_engine(
-        get_postgres_uri(),
-        isolation_level="REPEATABLE READ",
-    )
-)
-session = DEFAULT_SESSION_FACTORY()
+db = SQLAlchemy(app)
+
+class movies(db.Model):
+    movie_id = db.Column(db.Integer, primary_key=True)
+    preference_key = db.Column(db.Integer)
+    movie_title = db.Column(db.String)
+    rating = db.Column(db.Float)
+    year = db.Column(db.Integer)
+    create_time = db.Column(db.TIMESTAMP(timezone=True), index=True)
+    
+    def __init__(self, preference_key, movie_title, rating, year, create_time):   
+        self.preference_key = preference_key
+        self.movie_title = movie_title
+        self.rating = rating
+        self.year = year
+        self.create_time = create_time
+
+# DEFAULT_SESSION_FACTORY = sessionmaker(
+#     bind=create_engine(
+#         get_postgres_uri(),
+#         isolation_level="REPEATABLE READ",
+#     )
+# )
+# session = DEFAULT_SESSION_FACTORY()
 
 
 def main():
@@ -22,7 +46,7 @@ def main():
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
 
-    movies = soup.select('td.titleColumn')
+    moviesSoup = soup.select('td.titleColumn')
     links = [a.attrs.get('href') for a in soup.select('td.titleColumn a')]
     crew = [a.attrs.get('title') for a in soup.select('td.titleColumn a')]
     ratings = [b.attrs.get('data-value') for b in soup.select('td.posterColumn span[name=ir]')]
@@ -32,33 +56,40 @@ def main():
     # movie information
     list = []
 
-    # Iterating over movies to extract
+    # Iterating over moviesSoup to extract
     # each movie's details
-    for index in range(0, len(movies)):
+    for index in range(0, len(moviesSoup)):
         # Separating movie into: 'place',
         # 'title', 'year'
-        movie_string = movies[index].get_text()
+        movie_string = moviesSoup[index].get_text()
         movie = (' '.join(movie_string.split()).replace('.', ''))
         movie_title = movie[len(str(index)) + 1:-7]
         year = re.search('\((.*?)\)', movie_string).group(1)
         place = movie[:len(str(index)) - (len(movie))]
 
-        data = {"movie_title": movie_title,
-                "year": year,
-                "place": place,
-                "star_cast": crew[index],
-                "rating": ratings[index],
-                "vote": votes[index],
-                "link": links[index],
-                "preference_key": index % 4 + 1}
-        list.append(data)
+        # data = {"movie_title": movie_title,
+        #         "year": year,
+        #         "place": place,
+        #         "star_cast": crew[index],
+        #         "rating": ratings[index],
+        #         "vote": votes[index],
+        #         "link": links[index],
+        #         "preference_key": index % 4 + 1}
 
-    fields = ["preference_key", "movie_title", "star_cast", "rating", "year", "place", "vote", "link"]
-    with open("movie_results.csv", "w", newline="") as file:
-        writer = csv.DictWriter(file, fieldnames=fields)
-        writer.writeheader()
-        for movie in list:
-            writer.writerow({**movie})
+        movieObj = movies(index % 4 + 1, movie_title, ratings[index], year, datetime.now())
+        db.session.add(movieObj)
+        db.session.commit()
+        
+        # list.append(data)
+
+    # fields = ["preference_key", "movie_title", "star_cast", "rating", "year", "place", "vote", "link"]
+    # print(list[0]['movie_title'])
+    # return list[0]
+    # with open("movie_results.csv", "w", newline="") as file:
+    #     writer = csv.DictWriter(file, fieldnames=fields)
+    #     writer.writeheader()
+    #     for movie in list:
+    #         writer.writerow({**movie})
 
 if __name__ == '__main__':
     main()
