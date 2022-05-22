a689de131b58543e03357a848877f760aa6ddb97
betorendon <betorendonruiz@gmail.com>
2022-05-22 17:07:32 -0500

    Transformed preference key gen to class

--- src/preferenceKeyGen.py
+++ src/preferenceKeyGen.py
@@ -1,9 +1,13 @@
 from random import randint
 
-def preference_key_gen(preference_1, preference_2, preference_3):
-    pref_key = ((preference_1 * preference_2 * preference_3) % 5) + 1
-    
-    # Movie list does not have 5 as a preference key
-    if(pref_key == 5): pref_key -= 1
 
-    return pref_key
+# S - Single responsibility: Handles generating the preference key
+class PreferenceAlgorithm():
+    def keyGenerator(preference_1, preference_2, preference_3):
+        pref_key = ((preference_1 * preference_2 * preference_3) % 5) + 1
+
+        # Movie list does not have 5 as a preference key
+        if(pref_key == 5):
+            pref_key -= 1
+
+        return pref_key
